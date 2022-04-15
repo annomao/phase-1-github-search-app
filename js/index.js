1d3ff2d699cc4221f32d20646e637e414d6669e8
annomao <omaoannkerubo@gmail.com>
2022-04-15 20:58:07 +0300

    add function to get repo list of clicked user

--- js/index.js
+++ js/index.js
@@ -21,8 +21,12 @@ function getUser(){
   })
 //username, avatar and a link to their profile.
 }
+
 function displayUser(users){
   let ul = document.querySelector("#user-list")
+  ul.innerHTML=""
+  let repoUl = document.querySelector("#repos-list")
+  repoUl.innerHTML = ""
   users.forEach(user => {
     let li = document.createElement("li")
     let name = document.createElement("h2")
@@ -33,5 +37,33 @@ function displayUser(users){
     avatar.src = user.avatar_url
     li.appendChild(avatar)
     ul.appendChild(li)
+
+    //repos event listener
+    li.addEventListener("click",(event)=>{
+      let repoUrl = user.repos_url
+      fetch(repoUrl,{
+        method: "GET",
+        headers:{
+          Accept: "application/vnd.github.v3+json"
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        displayRepos(data)
+      })
+    })
   });
+}
+
+function displayRepos(repoList){
+  let repoUl = document.querySelector("#repos-list")
+  repoUl.innerHTML = ""
+  let h2 = document.createElement("h2")
+  h2.innerText = "Repositories of clicked user"
+  repoUl.appendChild(h2)
+  repoList.forEach(repo => {
+    let repoName = document.createElement("li")
+    repoName.innerText = repo.name
+    repoUl.appendChild(repoName)
+  })
 }
